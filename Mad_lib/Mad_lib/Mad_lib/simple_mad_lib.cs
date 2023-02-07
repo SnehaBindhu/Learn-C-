ee0a587b1298b1ff22878837f6e4be41b4ff91dd
Sneha Ramesh <sneha.ramesh@bjss.com>
2023-02-07 12:20:43 +0000

    mad lib app

---
+++ Mad_lib/Mad_lib/Mad_lib/simple_mad_lib.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mad_lib
+{
+    class simple_mad_lib
+    {
+        static void Main(string[] args)
+        {
+            string color, pluralnoun, person;
+
+            Console.Write("Enter a color:");
+            color = Console.ReadLine();
+
+            Console.Write("Enter a pluralnoun:");
+            pluralnoun = Console.ReadLine();
+
+            Console.Write("Enter a person name:");
+            person = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Sunflowers are "+ color);
+            Console.WriteLine(pluralnoun + " are blue");
+            Console.WriteLine("I love " +person);
+
+            Console.ReadLine();
+        }
+    }
+}
