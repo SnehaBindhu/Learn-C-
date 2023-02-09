ce014fe981628f651b9947f3fe7f2a2f5694de8c
Sneha Ramesh <sneha.ramesh@bjss.com>
2023-02-09 11:47:40 +0000

    add arrays file

---
+++ Arrays/Arrays/Array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Array
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            ints[2] = 11;
+
+            String[] weekDays;
+            weekDays = new string[4] { "Sun", "Mon", "Tue", "Wed" };
+            //weekDays[4] = "Thurs";
+
+            Console.WriteLine(ints[7]);
+            Console.WriteLine(weekDays[2]);
+            Console.ReadLine();
+        }
+    }
+}
